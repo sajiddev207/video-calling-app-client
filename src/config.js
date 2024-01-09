14f0ff2b0704d6e7319c6ad3e3f61c952a11e931
sajiddev207 <sajiddev207@gmail.com>
2024-01-09 17:31:05 +0530

    second

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const config = {
+    url: "http://43.204.142.206:8080",
     // url: "https://videocalling.winners11.in",
-    url: "http://localhost:8080",
+    // url: "http://localhost:8080",
     fcmEndpoint: 'https://fcm.googleapis.com/fcm/send',
     ICON: 'https://firebasestorage.googleapis.com/v0/b/kuberpe-4ff3f.appspot.com/o/logo.png?alt=media&token=ba8229a1-995c-4395-8bb7-aff5ce6d26e4',
     FCM_SERVER_KEY: 'AAAAOD9FeUA:APA91bGwohCoB8IzjfdsJxCbPGldHzNQ9h5sEeER4HUXjVAQkf0t7clYP849coJ20cAI8EOfG3in2XLIOXpe5Xg_ta8ilYMfk2KNuuzBdFT9Ml7noqtSz37m3OTFOoxxdZqapVlt-SkF',
